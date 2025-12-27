d44f64ef4f37e3eca1813ee12a70d2147364aafc
Mehmetalp17 <mehmetalpatay@gmail.com>
2025-12-27 23:13:04 +0300

    Configure Mailgun for EU region

--- backend/src/utils/emailService.js
+++ backend/src/utils/emailService.js
@@ -5,7 +5,8 @@ import Mailgun from 'mailgun.js';
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
     username: 'api',
-    key: process.env.MAILGUN_API_KEY || ''
+    key: process.env.MAILGUN_API_KEY || '',
+    url: 'https://api.eu.mailgun.net'
 });
 
 const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN || '';
